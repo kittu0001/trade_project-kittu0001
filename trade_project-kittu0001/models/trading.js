66340543e98b74c03e5d760a8e45551a604e5bb3
kittu0001 <keshavkittu9@gmail.com>
2023-04-25 21:35:20 -0400

    Milestone 4

---
+++ trade_project-kittu0001/models/trading.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const offerTradeschema = new Schema({
+  Name: { type: String, required: [true, "ItemName is required"] },
+  Category: { type: String, required: [true, "Item Category is required"] },
+  SavedBy: { type: Schema.Types.ObjectId, ref: "user" },
+  Status: { type: String },
+});
+
+const offerItem = mongoose.model("offer", offerTradeschema);
+
+module.exports = offerItem;
